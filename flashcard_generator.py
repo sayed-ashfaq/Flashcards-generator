fabc0150825e1e313a6056bcd14c598d2e35e74b
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-06-19 14:13:14 +0530

    Update flashcard_generator.py

--- flashcard_generator.py
+++ flashcard_generator.py
@@ -1,1 +1,13 @@
 print('This is to build the Flash Card generator to prepare well for the topics')
+
+# testing for the flask
+from flask import Flask
+
+print("is this app running")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
